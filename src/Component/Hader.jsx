b8dac83cd861d39133fad06ad7e2baf4d3f7c305
Krish25V <krishviramgama25@gmail.com>
2025-01-01 11:29:37 +0530

    responsive

--- src/Component/Hader.jsx
+++ src/Component/Hader.jsx
@@ -1,14 +1,25 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../assets/images/new-home/buradon-logo.png";
 import Image from "next/image";
 import { HiOutlineLocationMarker } from "react-icons/hi";
-import { FaFacebook, FaTwitterSquare, FaYoutube } from "react-icons/fa";
+import { FaBox, FaFacebook, FaHome, FaTwitter, FaTwitterSquare, FaUserCircle, FaYoutube } from "react-icons/fa";
 import { BiSolidPhoneCall } from "react-icons/bi";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import Link from "next/link";
+import { TbCertificate } from "react-icons/tb";
+import { MdContactPage } from "react-icons/md";
+import { RiContactsFill } from "react-icons/ri";
+import { HiInformationCircle } from "react-icons/hi";
+
+
 
 const Hader = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   // Sticky functionality
   const isSticky = () => {
     const header = document.querySelector(".header-lower");
@@ -69,6 +80,12 @@ const Hader = () => {
           <div className="auto-container">
             <div className="outer-box">
               <div className="menu-area clearfix">
+                <div className="mobile-nav-toggler" onClick={handleShow}>
+                  <i className="icon-bar" />
+                  <i className="icon-bar" />
+                  <i className="icon-bar" />
+                </div>
+
                 <nav className="main-menu navbar-expand-md navbar-light">
                   <ul className="navigation clearfix">
                     <li className="current">
@@ -84,7 +101,9 @@ const Hader = () => {
                       <Link href="/Certificate">Certification</Link>
                     </li>
                     <li>
-                      <Link href="/GeneralInformation">General Information</Link>
+                      <Link href="/GeneralInformation">
+                        General Information
+                      </Link>
                     </li>
                     <li>
                       <Link href="/ContactUs">Contact Us</Link>
@@ -163,6 +182,94 @@ const Hader = () => {
             </div>
           </div>
         </div>
+        {/* // mobile headar ===>  */}
+        <Offcanvas
+          className="mobileHeadar"
+          placement="end"
+          show={show}
+          onHide={handleClose}
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>
+              {" "}
+              <div className="nav-logo">
+                <a href="/">
+                  <Image src={Logo} alt="logo" className="mobileLogo" />
+                </a>
+              </div>
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+
+          <Offcanvas.Body>
+            <div>
+              <div className="mobilHader">
+                <Link href="/"> <FaHome className="mobilHaderIcon" /> Home</Link>
+              </div>
+              <div className="mobilHader">
+                <Link href="/AboutUs"><FaUserCircle className="mobilHaderIcon" />Profile</Link>
+              </div>
+              <div className="mobilHader">
+                <Link href="/ProductList"><FaBox className="mobilHaderIcon" />Products</Link>
+              </div>
+              <div className="mobilHader">
+                <Link href="/Certificate"><TbCertificate className="mobilHaderIcon" />Certification</Link>
+              </div>
+              <div className="mobilHader">
+                <Link href="/GeneralInformation"><HiInformationCircle className="mobilHaderIcon" />General Information</Link>
+              </div>
+              <div className="mobilHader">
+                <Link href="/ContactUs"><MdContactPage className="mobilHaderIcon" />Contact Us</Link>
+              </div>
+              <div className="mobilHader">
+                <Link href="/GetQote"><RiContactsFill className="mobilHaderIcon" />Get A Quote</Link>
+              </div>
+            </div>
+            <div>
+              <div className="contact-info">
+                <h4>Contact Info</h4>
+                <ul>
+                  <li>
+                    Block No: 429, Village: Umraya-391440 Taluka: Padra Dist:
+                    Vadodara Gujarat (INDIA)
+                  </li>
+                  <li>
+                    <a href="tel:+91-7486020636">+91-7486020636</a>
+                  </li>
+                  <li>
+                    <a href="tel:+91-7574886714">+91-7574886714</a>
+                  </li>
+                  <li>
+                    <a href="mailto:sales@buradon.com">sales@buradon.com</a>
+                  </li>
+                  <li>
+                    <a href="mailto:info@buradon.com">info@buradon.com</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="social-links">
+                <ul className="socialIcon">
+                  <li>
+                    <a href="#">
+                      <FaTwitter className="fab fa-twitter" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <FaFacebook className="fab fa-facebook-square" />
+                    </a>
+                  </li>
+                  {/* <li><a href="index.html"><span class="fab fa-pinterest-p"></span></a></li>
+                  <li><a href="index.html"><span class="fab fa-instagram"></span></a></li> */}
+                  <li>
+                    <a href="#">
+                      <FaYoutube className="fab fa-youtube" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </Offcanvas.Body>
+        </Offcanvas>
       </header>
     </React.Fragment>
   );
