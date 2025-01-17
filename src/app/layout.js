551b1dc04b9ee8db16a0d415f91963b973d5d5af
Krish25V <krishviramgama25@gmail.com>
2025-01-17 14:58:07 +0530

    design changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,25 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import Head from "next/head";
-import "../assets/css/style.css"
-import "../assets/css/color.css"
-import "../assets/css/animate.css"
-import "../assets/css/flaticon.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "../assets/css/style.css";
+import "../assets/css/color.css";
+import "../assets/css/animate.css";
+import "../assets/css/flaticon.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "../assets/css/responsive.css"
-import "../assets/css/flaticon.css"
+import "../assets/css/responsive.css";
+import "../assets/css/flaticon.css";
 import "./globals.css";
 
+// Load fonts using Next.js font optimization
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ["latin"], // Subset to use
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  subsets: ["latin"], // Subset to use
 });
 
 export const metadata = {
@@ -30,28 +31,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-whatever-integrity-key"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          charset="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Saira:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-
+        {/* No need to import fonts here since we're using next/font/google */}
         <link
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
           rel="stylesheet"
